4c9393a2a9ebfe5eac434bfbff56b32f4693df4a
John Kueh <yoongfook@gmail.com>
2019-07-10 22:23:47 +1000

    Refactor into use-form

--- web/pages/signup.js
+++ web/pages/signup.js
@@ -3,23 +3,28 @@ import Router from 'next/router';
 import gql from 'graphql-tag';
 import Cookies from 'js-cookie';
 import { useMutation } from 'react-apollo-hooks';
-import { useFormik } from '../hooks/use-formik';
+import { useForm } from '../hooks/use-form';
 import NavLink from '../components/nav-link';
 import Button from '../components/button';
 import AlertMessages from '../components/alert-messages';
 
 const Signup = () => {
   const signup = useMutation(SIGNUP);
-  const Formik = useFormik({
-    initialValues: { email: '', name: '', password: '' },
-    onSubmit: async input => {
+  const { formProps, fieldProps, errors, submitting } = useForm({
+    initialValues: {
+      email: '',
+      name: '',
+      password: ''
+    },
+    onSubmit: async ({ currentValues, setSubmitting }) => {
       const {
         data: {
           signup: { jwt }
         }
       } = await signup({
-        variables: { input }
+        variables: { input: currentValues }
       });
+      setSubmitting(false);
       Cookies.set('jwt', jwt);
       Router.push('/start');
     }
@@ -28,17 +33,21 @@ const Signup = () => {
   return (
     <>
       <h2>Sign up</h2>
-      <Formik.Form>
-        <AlertMessages messages={{ warning: Formik.errors }} />
-        <Formik.Field name="email" type="email" placeholder="Email address" />
-        <Formik.Field name="password" type="password" placeholder="Password" />
-        <Formik.Field name="name" type="text" placeholder="Name" />
-        <Button loading={Formik.isSubmitting} loadingText="Submitting..." type="submit">
+      <form {...formProps()}>
+        <AlertMessages messages={{ warning: errors }} />
+        <label>Email</label>
+        <input {...fieldProps('email')} type="email" placeholder="john@doe.com" />
+        <label>Password</label>
+        <input {...fieldProps('password')} type="password" placeholder="Your password" />
+        <label>Name</label>
+        <input {...fieldProps('name')} type="text" placeholder="John Doe" />
+        <Button loading={submitting} loadingText="Submitting..." type="submit">
           Sign up
         </Button>
-        <p>Have an account?&nbsp;</p>
-        <NavLink href="/login">Log in</NavLink>
-      </Formik.Form>
+        <p>
+          <NavLink href="/login">Log in</NavLink>
+        </p>
+      </form>
     </>
   );
 };

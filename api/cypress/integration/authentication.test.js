1abd0d20b725a6aa2265031124c906336156926b
John Kueh <yoongfook@gmail.com>
2019-07-11 23:03:29 +1000

    Setup cypress properly

--- api/cypress/integration/authentication.test.js
+++ api/cypress/integration/authentication.test.js
@@ -1,5 +1,5 @@
 describe('logging in', () => {
-  it('wrong credentials', () => {
+  it('with wrong credentials shows error messages', () => {
     cy.visit('/login');
 
     cy.get('[data-testid=email]').type('wrong@doe.com');
@@ -11,10 +11,12 @@ describe('logging in', () => {
       'Please check your credentials and try again.'
     );
   });
+
+  it('with correct credentials is successful', () => {});
 });
 
 describe('signing up', () => {
-  it('invalid fields', () => {
+  it('with invalid fields shows error messages', () => {
     cy.visit('/signup');
 
     cy.get('[data-testid=submit]').click();
@@ -35,10 +37,15 @@ describe('signing up', () => {
 
     cy.get('[data-testid=submit]').click();
     cy.get('[data-testid="alerts"]').should('contain', 'Password must be at least 6 characters');
+  });
 
-    cy.get('[data-testid=password]')
-      .clear()
-      .type('testpassword');
+  it('with valid fields is successful', () => {
+    cy.task('deleteUser', 'john@doe.com');
+    cy.visit('/signup');
+
+    cy.get('[data-testid=name]').type('John Doe');
+    cy.get('[data-testid=email]').type('john@doe.com');
+    cy.get('[data-testid=password]').type('testpassword');
     cy.get('[data-testid=submit]').click();
     cy.url().should('include', '/start');
   });
--- api/cypress/plugins/index.js
+++ api/cypress/plugins/index.js
@@ -1,17 +1,30 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+require('@babel/register')({
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ]
+});
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const Knex = require('knex');
+const { Model } = require('objection');
+const connection = require('../../knexfile');
+const User = require('../../models/user').default;
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  const knexConnection = Knex(connection.development);
+  Model.knex(knexConnection);
+
+  on('task', {
+    deleteUser(email) {
+      return User.query()
+        .delete()
+        .where('email', email);
+    }
+  });
+};

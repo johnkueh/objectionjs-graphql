fe79042fbb88c645682080f3bfe2fafcc6a07014
John Kueh <yoongfook@gmail.com>
2019-07-16 22:27:31 +1000

    Added more tests

--- api/models/image.js
+++ api/models/image.js
@@ -4,6 +4,21 @@ class Image extends Model {
   static get tableName() {
     return 'images';
   }
+
+  static get validateUniqueness() {
+    return ['publicId'];
+  }
+
+  static get validationSchema() {
+    return this.yup.object().shape({
+      publicId: this.yup.string().required(),
+      imageableType: this.yup
+        .string()
+        .required()
+        .matches(/(UserLogo)/),
+      imageableId: this.yup.string().required()
+    });
+  }
 }
 
 export default Image;
--- api/test/integration/image-management.test.js
+++ api/test/integration/image-management.test.js
@@ -5,7 +5,7 @@ import factory from 'factory-girl';
 import request from '../support/request';
 import handler, { path } from '../../src/index';
 
-describe('Creating images', () => {
+describe('Upserting images', () => {
   let user;
   const query = `
     mutation($input: UpsertImageInput!) {
@@ -21,6 +21,73 @@ describe('Creating images', () => {
     user = await factory.create('user', {
       email: 'john@doe.com',
       password: 'password'
+    });
+  });
+
+  it('is not able to create an image with missing fields', async () => {
+    const res = await request({
+      handler,
+      apiPath: path,
+      cookies: [`jwt=${user.jwt}`],
+      query,
+      variables: {
+        input: {
+          caption: 'A test caption'
+        }
+      }
+    });
+
+    expect(res.errors[0].extensions.exception.errors).toEqual({
+      publicId: 'PublicId is a required field',
+      imageableType: 'ImageableType is a required field',
+      imageableId: 'ImageableId is a required field'
+    });
+  });
+
+  it('is not able to create an image with invalid fields', async () => {
+    const res = await request({
+      handler,
+      apiPath: path,
+      cookies: [`jwt=${user.jwt}`],
+      query,
+      variables: {
+        input: {
+          publicId: 'xxx-id',
+          imageableId: 'yyy-id',
+          imageableType: 'WeirdImage',
+          caption: 'A test caption'
+        }
+      }
+    });
+
+    expect(res.errors[0].extensions.exception.errors.imageableType).toContain(
+      'ImageableType must match the following'
+    );
+  });
+
+  it('is not able to create an image with existing publicId', async () => {
+    await factory.create('image', {
+      publicId: 'existing-public-id',
+      imageableType: 'UserLogo',
+      imageableId: user.id
+    });
+
+    const res = await request({
+      handler,
+      apiPath: path,
+      cookies: [`jwt=${user.jwt}`],
+      query,
+      variables: {
+        input: {
+          publicId: 'existing-public-id',
+          imageableId: 'yyy-id',
+          imageableType: 'UserLogo'
+        }
+      }
+    });
+
+    expect(res.errors[0].extensions.exception.errors).toEqual({
+      publicId: 'PublicId is already taken'
     });
   });
 
@@ -49,5 +116,38 @@ describe('Creating images', () => {
     const userLogo = await user.$relatedQuery('logo');
     expect(userLogo.publicId).toEqual('xxxpublicid');
     expect(userLogo.caption).toEqual('A test caption');
+  });
+
+  it('is able to update an existing image with valid fields, does not create a new image', async () => {
+    const image = await factory.create('image', {
+      publicId: 'existing-public-id',
+      imageableType: 'UserLogo',
+      imageableId: user.id
+    });
+
+    const res = await request({
+      handler,
+      apiPath: path,
+      cookies: [`jwt=${user.jwt}`],
+      query,
+      variables: {
+        input: {
+          id: image.id,
+          publicId: 'new-public-id',
+          imageableType: 'UserLogo',
+          imageableId: user.id
+        }
+      }
+    });
+
+    expect(res.data.upsertImage).toEqual({
+      id: image.id,
+      caption: null,
+      publicId: 'new-public-id'
+    });
+
+    const userLogo = await user.$relatedQuery('logo');
+    expect(userLogo.id).toEqual(image.id);
+    expect(userLogo.publicId).toEqual('new-public-id');
   });
 });
